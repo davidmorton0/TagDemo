d909bc581fdff144e49eb5a05328d0fdcfce01e7
David Morton <davidmorton0@gmail.com>
2019-09-10 12:31:25 +0000

    tag demo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Returns the page title
+  def full_title(page_title = '')
+    if page_title.empty?
+      page_title = "London"
+    end
+    page_title + " - London Info"
+  end
 end
